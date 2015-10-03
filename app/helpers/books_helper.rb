36f3c01cddea15d504e670800e4e6b44c94aeec4
Freika <iamfrey@gmail.com>
2015-10-04 01:45:10 +0600

    Add another one book.

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,9 +1,9 @@
 module BooksHelper
   def render_cover(book)
     if book.cover.present?
-      image_tag(book.cover)
+      image_tag(book.cover, class: 'cover')
     else
-      image_tag('empty_cover')
+      image_tag('empty_cover', class: 'cover')
     end
   end
 end
